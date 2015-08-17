d8d85892f9e5fbced410acb4bd0a4830a9e3c886
lisaswanson <lds1233@gmail.com>
2015-08-16 17:16:34 -0700

    init final

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,14 +8,19 @@ get '/' do
 end
 
 get '/school' do 
+	p params
     @school_name = params[:school]
     @school = School.find(@school_name)
+    p @school
+    @teachers = User.where(is_teacher: true)
+    p @teachers
     erb :school
 end
 
 get '/wishlist' do 
-    
-
+	@teacher_id = params[:teacher_id]
+	@teacher = User.find(@teacher_id)
+	# @wishlists = Wishlist.all
     erb :wishlist
 end
 
